fa9e2d7ed161a88332f94ff557c456530deeae0f
AgustinPatane <guguipatane@gmail.com>
2022-10-23 01:25:26 -0300

    Update pruebamapas.mjs

--- pruebamapas.mjs
+++ pruebamapas.mjs
@@ -1,31 +1,39 @@
 
-//  mapa -->  "https://app.cartes.io/maps/986cfcf5-1956-4255-91fa-ae5299e460d5?lat=-37.98689013347518&lng=-57.52990722656251&zoom=13"
+//  mapa -->  "https://app.cartes.io/maps/986cfcf5-1956-4255-91fa-ae5299e460d5"
 
-// 1) Escribir en terminal "npm init"
-// 2) Escribir en terminal: "npm i node-fetch"
-// 3) Agregar "import fetch from 'node-fetch'"
-// 4) Cambiar extension del archivo a ".mjs"
+// 1) "npm init"
+// 2) "npm i node-fetch"
 
-const API = "https://cartes.io/maps"
-const mapa = "/986cfcf5-1956-4255-91fa-ae5299e460d5?lat=-37.98689013347518&lng=-57.52990722656251&zoom=13"
-//fetch(API_URL).then(res => console.log(res))
+import fetch from 'node-fetch'
 
-import fetch from 'node-fetch';
+const API = "https://cartes.io/api/maps"
+const mapa = "/986cfcf5-1956-4255-91fa-ae5299e460d5"
 
-const cargarMapas = async() => {
-    try {
-        const mapas = await fetch(`${API}${mapa}`)
-        console.log(mapas)
-        const marcas = await fetch(`${API}${mapa}/markers`)
-        console.log(marcas)
+const linkMapas = `${API}${mapa}`
+const linkMarkers = `${API}${mapa}/markers`
 
-    } catch (error) {
-        console.log(error)
-    }
+const markers = []
+
+const getMapas = async function(link){
+      fetch(link)
+      .then(async response => await response.json())
+      .then(async data => {
+            for(let i = 0; i < data.length; i++){
+                  
+                  let marca = new Object()
+                  marca.branchID = i + 1
+                  marca.longitud = await data[i].location.coordinates[0]
+                  marca.latitud = await data[i].location.coordinates[1]
+                  marca.nombre = await data[i].category.name 
+
+                  markers.push(marca)
+            }
+            console.log(markers)
+
+      })
 }
 
-cargarMapas();
-
+getMapas(linkMarkers)
 
 
 
