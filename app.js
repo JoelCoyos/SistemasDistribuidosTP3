e994186047b6ba92192c5fcd09b5f108c0e30f35
JoelCoyos <coyos52@gmail.com>
2022-11-04 20:56:29 -0300

    mostrar reservas disponibles

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 
+var app = new function () {
 
-async function MostrarReservas()
-{
-    const url = new URL("http://localhost:8080/api/reservas/")
-    fetch(url,{
-        method:'GET'
-    })
-    .then((response) => {
-        console.log(response);
-        /*if (!response.ok) {
-            throw new Error("HTTP status " + response.status);
-        }*/
-        return response.json();
-    })
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((error) => {
-       alert(error);
-    });
+    this.mostrarReservas =  function()
+    {
+        const url = new URL("http://localhost:8080/api/reservas/")
+        fetch(url,{
+            method:'GET'
+        })
+        .then((response) => {
+            console.log(response);
+            if (!response.ok) {
+                throw new Error("HTTP status " + response.status);
+            }
+            return response.json();
+        })
+        .then((reservas) => {
+            var data = '<br>';
+            if (reservas.length > 0) {
+              for (i = 0; i < reservas.length; i++) {
+                var fecha = reservas[i].date;
+                var sucursal = reservas[i].branchId;
+                var date = new Date(fecha);
+                data += '<tr>';
+                data += '<td id='+reservas[i].idTurno+'>Fecha: ' + date.toLocaleDateString() + ' Horario: '+ date.toLocaleTimeString() +' Sucursal: ' + sucursal +  '</td>'
+                data += '<td><button onclick="app.Reservar(' + i + ')">Reservar</button></td>';
+                data += '</tr>';
+              }
+            }
+            console.log("anda?");
+            document.getElementById('reservas').innerHTML = data;
+            document.getElementById('reservas').style.display = 'block';
+        })
+        .catch((error) => {
+           alert(error);
+        });
+    }
 
-        /*client.open("GET", "http://localhost:8080/api/reservas/", true);
-    client.responseType='text';
-    client.send();
-    client.onreadystatechange = () => {
-        if (client.readyState === client.HEADERS_RECEIVED) {
-          const contentType = client.getResponseHeader("Content-Type");
-          console.log(contentType);
-        }
-      }*/
 }
 
-MostrarReservas();
